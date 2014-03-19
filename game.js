fe239d6ed516f8dcbfe12d3f8887f6d67227953f
James Shieh <jshieh@linkedin.com>
2014-03-18 17:27:17 -0700

    formatting and style

--- game.js
+++ game.js
@@ -1,23 +1,23 @@
 //initialize empty home formation
 var home = new Array();
 for (var i = 0; i < 16; i++){
-  home.push(new tile('None', i%4,Math.floor(i/4)))
+  home.push(new Tile('None', i%4,Math.floor(i/4)))
 }
 
 //initialize empty visitor formation
 var visitor = new Array();
 for (var i = 0; i < 16; i++){
-  visitor.push(new tile('None', i%4,Math.floor(i/4)))
+  visitor.push(new Tile('None', i%4,Math.floor(i/4)))
 }
 
 //entire battlefield proto
-function battlefield(home, visitor){
-  this.home = new battlegrid(home)
-  this.visitor = new battlegrid(visitor)
+function BattleField(home, visitor){
+  this.home = new BattleGrid(home)
+  this.visitor = new BattleGrid(visitor)
 }
 
-//battlegrid proto
-function battlegrid(grid){
+//BattleGrid proto
+function BattleGrid(grid){
   this.grid = grid
   this.getTile = function getTile(x,y) {
    return this.grid[x+y*4]
@@ -30,14 +30,14 @@ function battlegrid(grid){
 }
 
 //tile proto
-function tile(entity, x, y) {
+function Tile(entity, x, y) {
   this.entity = entity
   this.x = x
   this.y = y
 }
 
 //entity proto
-function entity(type, damage, hp) {
+function Entity(type, damage, hp) {
   this.type = type
   if (type == 'Soldier') {
     this.attack_type = 'Melee'
