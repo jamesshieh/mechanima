486785347dbd0a5776ccb5a279be10b423550399
James Shieh <jshieh@linkedin.com>
2014-04-15 00:06:43 -0700

    added move function and test file

---
+++ test.js
@@ -0,0 +1,25 @@
+var left_formation = 
+[null,null,null,null,
+null,null,null,null,
+null,null,null,null,
+null,null,null,null];
+
+var right_formation  = 
+[null,null,null,null,
+null,null,null,null,
+null,null,null,null,
+null,null,null,null];
+
+var left_terrain = 
+["grass","grass","grass","grass",
+"grass","grass","grass","grass",
+"grass","grass","grass","grass",
+"grass","grass","grass","grass"];
+
+var right_terrain = 
+["grass","grass","grass","grass",
+"grass","grass","grass","grass",
+"grass","grass","grass","grass",
+"grass","grass","grass","grass"];
+
+var battle = new battleFactory({left_formation: left_formation, right_formation: right_formation, left_terrain: left_terrain, right_terrain: right_terrain});
