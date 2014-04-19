2ce6511f8ac9b83ea5a2119a4a62199b119b7b37
David Xu <dvdyxu@gmail.com>
2014-04-19 15:23:46 -0700

    rework equip to have one shared factory for all equip

--- equipment.js
+++ equipment.js
@@ -1,12 +1,16 @@
-function Equipment() {};
+function Equip() {
 
-Equipment.prototype.damage = function(weapontype) {
-  return weapontype();
 };
 
-equipmentFactory = function(options) {
-  var equipment = new Equipment();
+// expects options hash
+// option.stats: stat array
+// option.abilities: array of abilities granted by item
+// option.slot: equipment slot type used
+equipFactory = function(options) {
+  equip = new Equip();
+  equip.stats = [options.str, options.dex, options.int, options.vit];
+  equip.abilities = options.abilities;
+  equip.slot = option.slots;
 
-  return equipment;
-};
-
+  return equip;
+}
