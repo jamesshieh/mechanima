49ff2d9075c370d5588759fee1ac49f6adb3c800
James Shieh <jshieh@linkedin.com>
2014-04-21 22:12:25 -0700

    added davids updates to ability

--- ability.js
+++ ability.js
@@ -1,8 +1,49 @@
-function Ability() {
-  
-};
+abilityFactory = (function() {
+  var helpers = {
+    aoe: {
 
-abilityFactory = function(options) {
-  ability = new Ability():
-  ability.propertimes = options.properties;
-};
+    },
+
+    targets: {
+
+    },
+
+    action: {
+
+    }
+
+  }
+
+  function Ability() {
+    this.effects = [];
+  }
+  Ability.prototype.animate = function() {
+    console.log(this.animation);
+  }
+
+  function abilityFactory(options) {
+    var ability = new Ability();
+
+    // abilities options
+    // {
+    //   animation: { animation stuff... }
+    //   targets: target_helper_key,
+    //   effects: [
+    //     [aoe_helper_key_1, action_helper_key_1],
+    //     [aoe_helper_key_2, action_helper_key_2],
+    //     ...
+    //   ]
+    // }
+    ability.animation = options.animation
+    ability.targets = helpers.targets[options.aoe];
+    for (var i = 0; i < options.effects.length; i++) {
+      ability.effects.push({
+        aoe: options.aoe[0],
+        action: options.action[1]
+      });
+    }
+
+    return ability;
+  }
+  return abilityFactory;
+})();
