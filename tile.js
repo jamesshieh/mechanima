6ef23cf42145ef9217bc693f3c6576000c1031e8
James Shieh <jshieh@linkedin.com>
2014-04-21 22:13:05 -0700

    added new tile interface for state machine

--- tile.js
+++ tile.js
@@ -1,37 +1,66 @@
-//Tile Object
-function Tile() {
-  this.entity = null;
-  this.tile_sprite = null;
-};
+tileFactory = (function () {
+  //Tile Object
+  function Tile() {};
 
-//Set the tile entity
-Tile.prototype.set = function(entity) {
-  this.entity = entity;
-};
+  //rendering functions
+  Tile.prototype.activate = function() {
+    this.state = 1;
+  };
 
-//Get the tile entity
-Tile.prototype.get = function() {
-  return this.entity;
-};
+  Tile.prototype.render = function(context, offset_x, offset_y, reverse) {
+    switch(this.state) {
+      case 0: // default state
+      context.rect(offset_x, offset_y, 90, 90);
+      context.stroke();
+        break;
 
-//Clear current tile and return entity if occupied
-Tile.prototype.remove = function() {
-  curr = this.entity;
-  this.entity = null;
-  return curr;
-};
+      case 1: // activated state
+      console.log("cool");
+      context.fillStyle = "#FF0000";
+      context.fillRect(offset_x,offset_y,90,90);
+        break;
 
-//Is empty?
-Tile.prototype.empty = function() {
-  this.entity == null ? true : false;
-};
+    }
+  };
 
-//Tile Factory
-tileFactory = function(options) {
-  var tile = new Tile();
+  Tile.prototype.reset = function() {
+    this.state = 0;
+  };
 
-  tile.entity = options.entity;
-  tile.tile_sprite = options.tile_sprite;
+  //logic funtions
 
-  return tile
-};
+  //Set the tile entity
+  Tile.prototype.set = function(entity) {
+    this.entity = entity;
+  };
+
+  //Get the tile entity
+  Tile.prototype.get = function() {
+    return this.entity;
+  };
+
+  //Clear current tile and return entity if occupied
+  Tile.prototype.remove = function() {
+    curr = this.entity;
+    this.entity = null;
+    return curr;
+  };
+
+  //Is empty?
+  Tile.prototype.empty = function() {
+    this.entity == null ? true : false;
+  };
+
+  //Tile Factory
+  tileFactory = function(options) {
+    var tile = new Tile();
+
+    tile.state = 0;
+    tile.entity = options.entity || null;
+    tile.tile_sprite = options.tile_sprite || null;
+
+    return tile;
+  };
+
+  return tileFactory();
+})();
