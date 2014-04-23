03a38727bd65911d4e07d662ce9b8063669abae2
James Shieh <jshieh@linkedin.com>
2014-04-23 11:12:56 -0700

    adding basic targeting handlers

--- ability.js
+++ ability.js
@@ -1,10 +1,64 @@
 abilityFactory = (function() {
   var helpers = {
     aoe: {
-
+      columnThreeCleave: function(field, x, y) {
+        var valid;
+        field[y+1], field[y-1];
+      },
+      fullRowPierce: function(field, x, y) {
+        return helpers.targeting.getRow(field, x); 
+      }
+      
     },
 
     targets: {
+      //takes in opposite field and target x y
+      meleeSingleTarget: function(field, x, y) { 
+        var valid;
+        var targets = this.get_row(field, x);
+        for (tile in targets) {
+          valid.push(tile);
+          if (!tile.empty) {
+            break;
+          }
+        };
+        return valid;
+      },
+      //Ranged hit any in a row
+      rangedSingleTarget: function(field, x, y) {
+        return this.get_row(field, x);
+      },
+      meleeThreeCleave: function(field, x, y) {
+        var valid;
+      },
+      getRow: function(field, x) {
+        var row;
+        switch (x) 
+        {
+        case 1:
+          row = field.tiles.slice(0,3);
+          break;
+        case 2:
+          row = field.tiles.slice(4,7);
+          break;
+        case 3:
+          row = field.tiles.slice(8,11);
+          break;
+        case 4:
+          row = field.tiles.slice(12,15);
+          break;
+        };
+        return row;
+      },
+      getColumn: function(field, y) {
+        var column;
+        for (var i;i < field.tiles.length;i++) {
+          if (i % 4 == y) {
+            column.push(field.tiles[i]);
+          };
+        };
+        return column;
+      }
 
     },
 
