1d82be98cbdc689fef5d9e42e3337c716a54edb0
James Shieh <jshieh@linkedin.com>
2014-04-29 22:41:35 -0700

    moved targeting helpers

--- ability.js
+++ ability.js
@@ -2,14 +2,14 @@ abilityFactory = (function() {
   var helpers = {
     aoe: {
       meleeSingleTarget: function(field, x, y) { 
-        return field.tiles[x+y*4];
+        return this.targetingHelpers.getTile(field, x, y);
       },
       columnThreeCleave: function(field, x, y) {
         var valid;
         field.tiles[y+1], field.tiles[y], field.tiles[y-1];
       },
       fullRowPierce: function(field, x, y) {
-        return helpers.getRow(field, x); 
+        return targetingHelpers.getRow(field, x, y); 
       }
       
     },
@@ -18,11 +18,8 @@ abilityFactory = (function() {
       //takes in opposite field and target x y
       meleeSingleTarget: function(field, x, y) { 
         var valid = [];
-        console.log(this.helpers);
-        var targets = this.helpers.getRow(field, x);
-        console.log(targets);
-        for (i in targets) {
-          console.log(targets[i]);
+        var targets = this.targetingHelpers.getRow(field, x, y);
+        for (var i = 0; i < targets.length; i++) {
           valid.push(targets[i]);
           if (!targets[i].empty()) {
             break;
@@ -32,31 +29,19 @@ abilityFactory = (function() {
       },
       //Ranged hit any in a row
       rangedSingleTarget: function(field, x, y) {
-        return this.helpers.getRow(field, x);
+        return this.targetingHelpers.getRow(field, x, y);
       },
       //All Empty Tiles
-      emptyTiles: function(field) {
-        var valid = [];
-        for (i in field.tiles) {
-          if (field.tiles[i].empty()) {
-            valid.push(field.tiles[i]);
-          };
-        };
-        return valid;
+      emptyTiles: function(field, x, y) {
+        return getEmpty(field, x, y);
       },
       //All occupied Tiles
-      occupiedTiles: function(field) {
-        var valid = [];
-        for (i in field.tiles) {
-          if (!field.tiles[i].empty()) {
-            valid.push(field.tiles[i]);
-          };
-        };
-        return valid;
+      occupiedTiles: function(field, x, y) {
+        return getOccupied(field, x, y);
       },
       //All Tiles
-      allTiles: function(field) {
-        return field.tiles;
+      allTiles: function(field, x, y) {
+        return getAll(field, x, y);
       }
     },
 
@@ -64,14 +49,19 @@ abilityFactory = (function() {
       damage: function(dmg, target) {
         target.damage(dmg);
       },
-      move: function(field) {
-
+      move: function(field, aoe) {
+        var target = aoe(field, x, y);
+        if (target.empty) {
+          target.set(entity);
+          return true;
+        };
+        return false;
       }
     }
   };
 
-  var helpers2 = {
-    getRow: function(field, x) {
+  var targetingHelpers = {
+    getRow: function(field, x, y) {
       var row;
       switch (x) 
       {
@@ -90,7 +80,7 @@ abilityFactory = (function() {
       };
       return row;
     },
-    getColumn: function(field, y) {
+    getColumn: function(field, x, y) {
       var column;
       for (var i;i < field.tiles.length;i++) {
         if (i % 4 == y) {
@@ -98,15 +88,46 @@ abilityFactory = (function() {
         };
       };
       return column;
+    },
+    getTile: function(field, x, y) {
+      return field.tiles[x+y*4];
+    },
+    getAll: function(field, x, y) {
+      return field.tiles;
+    },
+    getEmpty: function(field, x, y) {
+      var valid = [];
+      for (var i = 0; i < field.tiles.length; i++) {
+        if (field.tiles[i].empty()) {
+          valid.push(field.tiles[i]);
+        };
+      };
+      return valid;
+    },
+    getOccupied: function(field, x, y) {
+      var valid = [];
+        for (var i = 0; i < field.tiles.length; i++) {
+          if (!field.tiles[i].empty()) {
+            valid.push(field.tiles[i]);
+          };
+        };
+      return valid;
     }
   };
   
 
   function Ability() {
     this.effects = [];
-  }
+  };
+
   Ability.prototype.animate = function() {
-  }
+  };
+
+  Ability.prototype.execute = function(field, c_x, c_y, t_x, t_y) {
+    for (var i = 0; i < this.effects.length; i++) {
+      this.effects[i].action(field, this.effects[i].aoe, c_x, c_t, t_x, t_y);
+    };
+  };
 
   function abilityFactory(options) {
     var ability = new Ability();
@@ -124,11 +145,11 @@ abilityFactory = (function() {
     // }
     ability.animation = options.animation;
     ability.targets = helpers.targets[options.targets];
-    ability.helpers = helpers2;
+    ability.targetingHelpers = targetingHelpers;
     for (var i = 0; i < options.effects.length; i++) {
       ability.effects.push({
-        aoe: options.effects[0],
-        action: options.effects[1]
+        aoe: helpers.aoe[options.effects[i][0]],
+        action: helpers.action[options.effects[i][1]]
       });
     };
 
